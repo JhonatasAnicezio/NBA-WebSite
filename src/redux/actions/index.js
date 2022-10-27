56d8efe4af838e43722708c452d939fc2c521392
Jhonatas <jhonatasanic@hotmail.com>
2022-10-27 17:53:42 -0300

    feat(index): add action getScore and fetchScores

---
+++ src/redux/actions/index.js
@@ -0,0 +1,16 @@
+const GET_SCORE = 'GET_SCORE';
+
+function getScore(json) {
+  return {
+    type: GET_SCORE,
+    payload: json,
+  }
+}
+
+export function fetchScores() {
+  return (dispatch) => {
+    const url = 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard';
+    return fetch(url).then((response) => response.json())
+      .then((json) => dispatch(getScore(json)));
+  }
+}
