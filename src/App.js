335cfa8463a628c51363adc5a59699804307b603
Jhonatas <jhonatasanic@hotmail.com>
2022-10-27 19:01:40 -0300

    feat(App): add dispatch in useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchScores } from './redux/actions';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchScores());
+  }, []);
+
   return (
     <div>
       NBA
