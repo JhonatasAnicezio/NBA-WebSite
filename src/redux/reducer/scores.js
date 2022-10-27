46ec7336e4d709e4762624e8d40375b99f11622b
Jhonatas <jhonatasanic@hotmail.com>
2022-10-27 13:15:55 -0300

    feat(reducer/score): create reducer score

---
+++ src/redux/reducer/scores.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = {
+  date: '',
+  events: [],
+  leagues: [],
+  season: {},
+};
+
+const scores = ( state =  INITIAL_STATE, action ) => {
+  switch(action.type) {
+  default:
+    return state;
+  }
+}
+
+export default scores;
