551f9ec565b38015e24611bfc093a5a040417901
Jhonatas <jhonatasanic@hotmail.com>
2022-10-28 13:31:40 -0300

    feat(pages/Home): render Header and Events

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Events from '../components/components-home/Events';
+import Header from '../components/Header';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
+    <div className='page-Home'>
+      <Header />
+      <Events />
       Home Page
     </div>
   );
