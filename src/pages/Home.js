53214148a6bf685c169a8ccbcb92df5ee8c3c397
Jhonatas <jhonatasanic@hotmail.com>
2022-11-01 11:20:15 -0300

    feat(pages/Home): .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import BannerNew from '../components/components-home/BannerNew';
 import Events from '../components/components-home/Events';
 import Header from '../components/Header';
+import { fetchWNews } from '../redux/actions';
 import './Home.css';
 
 function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchWNews());
+  }, []);
+
   return (
     <div className='page-Home'>
       <Header />
       <Events />
-      Home Page
+      <BannerNew />
     </div>
   );
 }
