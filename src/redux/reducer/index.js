a9c728b98987646d843b4cd10fb1bf04d5070355
Jhonatas <jhonatasanic@hotmail.com>
2022-11-01 11:16:15 -0300

    feat(reducer/index): add reducer news for rootreducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import scores from './scores';
+import news from './news';
 
-const rootReducer = combineReducers({ scores });
+const rootReducer = combineReducers({ scores, news });
 
 export default rootReducer;
