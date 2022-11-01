10d5ec4368a107dcdcc41257d370b7db4b081e51
Jhonatas <jhonatasanic@hotmail.com>
2022-11-01 11:18:29 -0300

    feat(components-home/BannerNew): create component BannerNew

---
+++ src/components/components-home/BannerNew.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect, useDispatch } from 'react-redux';
+import { fetchNews } from '../../redux/actions';
+import './BannerNew.css';
+
+function BannerNew({ firstNew }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchNews());
+  }, []);
+
+  return (
+    <div className='container-bannerNew'>
+      <img src={ firstNew.image } alt='image' />
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    articlesNBA: state.news.articlesNBA,
+    articlesWNBA: state.news.articlesWNBA,
+    firstNew: state.news.firstNew,
+  };
+}
+
+BannerNew.propTypes = {
+  articlesNBA: PropTypes.array.isRequired,
+  articlesWNBA: PropTypes.array.isRequired,
+  firstNew: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps)(BannerNew);
