517bfedda46c86f90bc081752eafdf4d9131b617
Jhonatas <jhonatasanic@hotmail.com>
2022-11-01 11:16:54 -0300

    feat(redux/actions): create actions for news

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,9 +1,11 @@
 export const GET_SCORE = 'GET_SCORE';
+export const GET_NBA_NEWS = 'GET_NBA_NEWS';
+export const GET_WNBA_NEWS = 'GET_WNBA_NEWS';
 
-function getScore(json) {
+function getScore(scores) {
   return {
     type: GET_SCORE,
-    scores: json,
+    scores,
   };
 }
 
@@ -12,5 +14,35 @@ export function fetchScores() {
     const url = 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard';
     return fetch(url).then((response) => response.json())
       .then((json) => dispatch(getScore(json)));
+  };
+}
+
+function getNbaNews(news) {
+  return {
+    type: GET_NBA_NEWS,
+    news,
+  };
+}
+
+export function fetchNews() {
+  return (dispatch) => {
+    const url = 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/news';
+    return fetch(url).then((response) => response.json())
+      .then((json) => dispatch(getNbaNews(json.articles)));
+  };
+}
+
+function getWnbaNews(news) {
+  return {
+    type: GET_WNBA_NEWS,
+    news,
+  };
+}
+
+export function fetchWNews() {
+  return (dispatch) => {
+    const url = 'http://site.api.espn.com/apis/site/v2/sports/basketball/wnba/news';
+    return fetch(url).then((response) => response.json())
+      .then((json) => dispatch(getWnbaNews(json.articles)));
   };
 }
