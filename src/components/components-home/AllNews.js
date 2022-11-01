738c02c5b52bc2350a7bd0914131fafbc4034a06
Jhonatas <jhonatasanic@hotmail.com>
2022-11-01 19:27:49 -0300

    feat(components-home/AllNews): create components AllNews

---
+++ src/components/components-home/AllNews.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import './AllNews.css';
+import CardNew from './CardNew';
+
+function AllNews({ articles }) {
+  return (
+    <div className='container-allNews'>
+      <h2>NEWS</h2>
+      <div className='container-news'>
+        { articles.map((article, index) => 
+          <CardNew
+            key={ index }
+            url={ article.images[0].url }
+            description={ article.headline }
+            link={ article.links.web.href }
+          />
+        ) }
+      </div>
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  const { news } = state;
+  return {
+    articles: news.articlesNBA.concat(news.articlesWNBA),
+  };
+}
+
+AllNews.propTypes = {
+  articles: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(AllNews);
