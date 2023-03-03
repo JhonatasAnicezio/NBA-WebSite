5226bc1c7253d9cb552ced0a8fbe301b0795a201
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-03-03 16:06:44 -0300

    finish

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className='container-footer'>
-      
+      <p>Desenvolvido por <a href='https://www.linkedin.com/in/jhonatas-anicezio/' target='blank'>Jhonatas Anicezio</a> &copy; 2023</p>
     </footer>
   );
 }
